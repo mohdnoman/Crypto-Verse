e8542a1ecd2106aa75c8a85223c33a0686418a79
mohd noman <mohdnoman2751@gmail.com>
2023-12-08 04:30:09 +0530

    created structure of website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Switch, Route,Link} from 'react-router-dom';
+import { Switch, Route, Link, Routes } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components';
-
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
+import './App.css';
 const App = () => {
     return(
         <div className='app'>
@@ -11,11 +11,30 @@ const App = () => {
                 <Navbar  />
            </div>
            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route exact path='/' element={<Homepage />}></Route>
+                            <Route exact path='/exchanges' element={<Exchanges />}></Route>
+                            <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />}></Route>
+                            <Route exact path='/crypto/:coinId' element={<CryptoDetails />}></Route>
+                            <Route exact path='/news' element={<News />}></Route>
+                        </Routes>
+                    </div>
+                </Layout>
 
-           </div>
            <div className='footer'>
-
-           </div>
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Cryptoverse <br />
+                        All rights reserved
+                    </Typography.Title>
+                    <Space >
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
+            </div>
         </div>
     )
 }
