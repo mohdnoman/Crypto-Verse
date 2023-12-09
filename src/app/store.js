577fbc5ab1892ae4759dd6b4e3fc9a17bbf96203
mohd noman <mohdnoman2751@gmail.com>
2023-12-10 05:18:12 +0530

    fetched crypto data and displaied crypto list.

--- src/app/store.js
+++ src/app/store.js
@@ -5,4 +5,6 @@ export default configureStore({
     reducer: {
         [cryptoApi.reducerPath]: cryptoApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(cryptoApi.middleware),
 });
