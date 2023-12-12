b32ecdc3adeaa6bedcff1c8172e7a9d5d6a3fec6
mohd noman <mohdnoman2751@gmail.com>
2023-12-13 05:11:48 +0530

    fetched News data.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
-import { Select, Typography, Row, Col, Avatar,Card } from 'antd';
+import { Select, Typography, Row, Col, Avatar, Card, Spin } from 'antd';
 import moment from 'moment';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
-const News = () => {
-    return(
-        <div>
-         News
-        </div>
-    )
-}
+const { Text, Title } = Typography;
+const { Option } = Select;
 
-export default News 
+const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
+const News = ({ simplified }) => {
+  const { data: cryptoNews, error, isLoading } = useGetCryptoNewsQuery({ count: simplified ? 6 : 12 });
+
+  if (isLoading) {
+    return <Spin size="large" />;
+  }
+
+  if (error) {
+    return <div>Error loading news data. Please try again later.</div>;
+  }
+
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews?.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className='news-card'>
+            <a href={news.Url} target='_blank' rel='noreferrer'>
+              <div className='news-image-container'>
+                <Title className='news-title' level={4}>
+                  {news.Title}
+                </Title>
+                <img src={news.Image || demoImage} alt='news' />
+              </div>
+              <p>
+                {news.Description.length > 100
+                  ? `${news.Summary}...`
+                  : news.Description}
+              </p>
+              <div className='provider-container'>
+                <Avatar src={news.Source || demoImage} alt='' />
+                <Text>{moment(news.PublishedOn).startOf('ss').fromNow()}</Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default News;
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,23 +1,48 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// const cryptoNewsHeaders = {
+//     // 'X-BingApis-SDK': 'true',
+//     // 'X-RapidAPI-Key': '8a398cada9msha379f18c0d677fep13b001jsnbcd8b52a6044',
+//     // 'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+//     'X-RapidAPI-Key': '8a398cada9msha379f18c0d677fep13b001jsnbcd8b52a6044',
+//     'X-RapidAPI-Host': 'news67.p.rapidapi.com'
+// }
+
+// const baseUrl = 'https://news67.p.rapidapi.com/v2/crypto';
+// // 'https://bing-news-search1.p.rapidapi.com'
+
+// const createRequest = (url) => ({ url, headers: cryptoNewsHeaders })
+
+// export const cryptoNewsApi = createApi({
+//     reducerPath: 'cryptoNewsApi',
+//     baseQuery: fetchBaseQuery({ baseUrl }),
+//     endpoints: (builder) => ({
+//         getCryptoNews: builder.query({
+//             query: ({ count }) => createRequest('')
+//         })
+//     })
+// });
+
+// export const {useGetCryptoNewsQuery} = cryptoNewsApi;
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeaders = {
-    'X-BingApis-SDK': 'true',
     'X-RapidAPI-Key': '8a398cada9msha379f18c0d677fep13b001jsnbcd8b52a6044',
-    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
-}
+    'X-RapidAPI-Host': 'news67.p.rapidapi.com'
+};
 
-const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+const baseUrl = 'https://news67.p.rapidapi.com/v2/crypto';
 
-const createRequest = (url) => ({ url, headers: cryptoNewsHeaders })
+const createRequest = (url, params) => ({ url, headers: cryptoNewsHeaders, params });
 
 export const cryptoNewsApi = createApi({
     reducerPath: 'cryptoNewsApi',
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptoNews: builder.query({
-            query: ({newsCategory ,count}) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
-        })
-    })
+        query: ({ count }) => createRequest('', { count }), // Pass count as a query parameter
+    }),
+    }),
 });
 
-export const {useGetCryptoNewsQuery} = cryptoNewsApi;
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
